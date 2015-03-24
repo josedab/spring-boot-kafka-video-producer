0115968d7ae233f9f64806913043f104f97250d2
Jose David Baena <josedab@gmail.com>
2015-03-24 22:53:45 +0000

    Added object serialisation and used KeyedMessage to transport the data through Kafka

--- src/main/java/com/utiltube/kafka/video/producer/VideoProducer.java
+++ src/main/java/com/utiltube/kafka/video/producer/VideoProducer.java
@@ -10,17 +10,26 @@ import kafka.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.utiltube.kafka.config.ProducerConfigFactory;
+import com.utiltube.kafka.video.model.Video;
 
 @Component
 public class VideoProducer {
     
-    private static final String TOPIC = "test";
+    private static final String TOPIC = "video_test";
 
     @Autowired
     private ProducerConfigFactory producerConfigFactory;
     
     private Producer<String, String> producer;
+    private ObjectMapper objectMapper;
+    
+    public VideoProducer() {
+        objectMapper = new ObjectMapper();
+    }
+    
     @PostConstruct
     public void startProducing() {
         ProducerConfig producerConfig = producerConfigFactory.getProducerConfig();
@@ -31,12 +40,23 @@ public class VideoProducer {
     
     private void produce() {
         for (int i = 0; i < 100; i++) {
-            KeyedMessage<String, String> data = 
-                            new KeyedMessage<String, String>(
-                                    TOPIC, 
-                                    String.valueOf(i), 
-                                    "Testing producer message " + i);
-            producer.send(data);
+            Video video = new Video.Builder("ident" + i, "local", "Title for ident_" + i)
+                                   .build();
+            try {
+                
+                String messageData = objectMapper.writeValueAsString(video);
+                KeyedMessage<String, String> data = 
+                        new KeyedMessage<String, String>(
+                                TOPIC, 
+                                String.valueOf(i), 
+                                messageData);
+                producer.send(data);
+                
+            } catch (JsonProcessingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
         }
         producer.close();
     }
